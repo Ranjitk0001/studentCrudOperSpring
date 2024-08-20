d8bde2b3822536afbe35d22f9c02e6b2fa3471b2
rk <ranjitkale0001@gmail.com>
2024-08-20 17:55:04 +0530

    DATABASE ACCESS TOKEN  CRUD OPERATION
    goto chrome
    
    http://localhost:8085/swagger-ui.html
    
    LOGIN
    
    {
      "password": "pass",
      "userName": "kale@gmail.com"
    }
    
    ACCESS TOKEN

--- studentCrudOperSpring/src/main/java/org/javapatil/controller/StudentController.java
+++ studentCrudOperSpring/src/main/java/org/javapatil/controller/StudentController.java
@@ -2,8 +2,12 @@ package org.javapatil.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.javapatil.model.StudentForm;
 import org.javapatil.services.StudentServices;
+import org.javapatil.util.security.admin.BaseResource;
+import org.javapatil.util.security.admin.User;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,6 +16,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,38 +24,63 @@ import lombok.AllArgsConstructor;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping(value = "students")
+@RequestMapping(value = "v1/admin/students")
 //Connect to Angular
 @CrossOrigin("*")
-public class StudentController {
+public class StudentController extends BaseResource {
 	private StudentServices studentServices;
 
 	@GetMapping
-    public List<StudentForm>	getStudListCtrl(){
+    public List<StudentForm>	getStudListCtrl(@RequestHeader("access-token") String token, HttpServletRequest request)
+	{
+		User user = getUser(request);
+
+	     if(user==null) return null;
+
+		
 		List<StudentForm> studList=studentServices.getStudListService();
 	 
 	 return studList;
 	}
 	
 	@PostMapping
-	public void insertStudCtrl(@RequestBody StudentForm studentForm) {
+	public void insertStudCtrl(@RequestBody StudentForm studentForm,@RequestHeader("access-token") String token, HttpServletRequest request) {
+		User user = getUser(request);
+
+	     if(user==null) return;
+		
 		studentServices.insertStudServices(studentForm);
 	}
 	
 	@GetMapping("/{snum}")
-	public StudentForm getStudBySnoCtrl(@PathVariable(value = "snum") int sno) {
+	public StudentForm getStudBySnoCtrl(@PathVariable(value = "snum") int sno,@RequestHeader("access-token") String token, HttpServletRequest request)
+	{
+		User user = getUser(request);
+
+	     if(user==null) return null;
+		
 		StudentForm studentForm=studentServices.getStudBySnoService(sno);
 		return studentForm;
 	}
 
 	@PutMapping
-	public void updateStudCtrl(@RequestBody StudentForm studentForm) {
+	public void updateStudCtrl(@RequestBody StudentForm studentForm,@RequestHeader("access-token") String token, HttpServletRequest request)
+	{
+		User user = getUser(request);
+
+	     if(user==null) return;
+	     
 		studentServices.updateStudServices(studentForm);
 	}
 	
 	
 	@DeleteMapping("/{snum}")
-	public void deleteStudCtrl(@PathVariable(value = "snum") int sno) {
+	public void deleteStudCtrl(@PathVariable(value = "snum") int sno ,@RequestHeader("access-token") String token, HttpServletRequest request)
+	{ 
+		User user = getUser(request);
+
+	     if(user==null) return;
+	     
 		studentServices.deleteStudServices(sno);
 	}
 }
