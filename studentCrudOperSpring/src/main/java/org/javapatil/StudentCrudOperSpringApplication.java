bcecf6f519dbf0376712807dfb61bb4d8d9cebee
rk <ranjitkale0001@gmail.com>
2024-08-20 10:28:01 +0530

    intial
    start the project and github upload

---
+++ studentCrudOperSpring/src/main/java/org/javapatil/StudentCrudOperSpringApplication.java
@@ -0,0 +1,13 @@
+package org.javapatil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentCrudOperSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentCrudOperSpringApplication.class, args);
+	}
+
+}
---
+++ studentCrudOperSpring/src/test/java/org/javapatil/StudentCrudOperSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package org.javapatil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentCrudOperSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
